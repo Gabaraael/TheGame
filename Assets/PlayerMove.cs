fdfd48f3811b58eeeb81036f68201f7ba21ba479
Gabaraael <Gabriel1998.gp45@gmail.com>
2022-10-17 01:37:09 -0300

    Player move

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rgbd2d;
+    Vector3 movementVector;
+
+    [SerializeField] float speed = 3f;
+
+    void Awake() 
+    {
+        rgbd2d = GetComponent<Rigidbody2D>();
+        movementVector = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+
+        movementVector *= speed;
+
+        rgbd2d.velocity = movementVector;
+        
+    }
+}
